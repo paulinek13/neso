798938843a755247d30d949fbfaa1118ba53c1de
paulinek13 <paulinek13@outlook.com>
2023-08-16 16:35:58 +0200

    added MainFooter component

---
+++ src/components/MainFooter.svelte
@@ -0,0 +1,12 @@
+<div
+    class="px-8 py-4 w-full border-t border-stone-800 flex flex-col gap-1 justify-center items-center"
+>
+    <div class="px-4 text-sm py-1 text-left">
+        <span class="font-extrabold">Neso</span> -
+        <a
+            href="https://solarsystem.nasa.gov/moons/neptune-moons/neso/in-depth/"
+            class="text-blue-500 italic font-normal"
+            >"another of Neptune's extremely distant irregular moons"</a
+        >
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+    import MainFooter from "../components/MainFooter.svelte";
+</script>
+
 <div class="w-full flex flex-col">
     <div class="flex-1 flex flex-col justify-center items-center gap-4">
         <span class="flex-1" />
@@ -32,17 +36,6 @@
             >
         </div>
         <span class="flex-1" />
-        <div
-            class="px-8 py-4 w-full border-t border-stone-800 flex flex-col gap-1 justify-center items-center"
-        >
-            <div class="px-4 text-sm py-1 text-left">
-                <span class="font-extrabold">Neso</span> -
-                <a
-                    href="https://solarsystem.nasa.gov/moons/neptune-moons/neso/in-depth/"
-                    class="text-blue-500 italic font-normal"
-                    >"another of Neptune's extremely distant irregular moons"</a
-                >
-            </div>
-        </div>
+        <MainFooter />
     </div>
 </div>
