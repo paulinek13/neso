7cf5e2c2d49d9bbedb59550b229faf900901b3da
paulinek13 <paulinek13@outlook.com>
2023-09-12 18:57:09 +0200

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<div class="w-full flex flex-col justify-center items-center">
+    <h1 class="font-extrabold font-mono text-6xl text-orange-600">
+        {$page.status}
+    </h1>
+    <h1 class="lowercase">{$page.error.message}</h1>
+</div>
