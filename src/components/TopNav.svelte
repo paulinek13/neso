c9e2d074ec9a45e1a1a3fc432da32a165d1a7d98
paulinek13 <paulinek13@outlook.com>
2023-09-12 19:04:19 +0200

    added separate component for top nav

---
+++ src/components/TopNav.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { slide } from "svelte/transition";
+
+    let expanded = false;
+</script>
+
+<div
+    class="sticky top-0 bg-stone-900 border-b border-stone-800 bg-opacity-80 bg-clip-padding shadow-sm shadow-neutral-950 backdrop-blur-sm backdrop-filter z-50 text-neutral-400 flex flex-col p-4"
+>
+    <div class="flex flex-wrap gap-4 justify-center items-center">
+        <div class="flex flex-wrap gap-4 items-center justify-center">
+            <a href="/" class="text-blue-600 font-medium">home</a>
+            <a href="/apod/today">today</a>
+            <a href="/apod/day">day</a>
+            <a href="/apod/random">random</a>
+            <a href="/apod/calendar">calendar</a>
+        </div>
+        <button
+            on:click={() => {
+                expanded = !expanded;
+            }}
+            class="active:bg-stone-800 text-orange-500">☰</button
+        >
+    </div>
+
+    {#if expanded}
+        <div
+            transition:slide
+            class="flex flex-wrap gap-4 pt-4 items-center justify-center"
+        >
+            <a
+                class="border border-stone-700"
+                href="https://github.com/paulinek13/neso"
+                target="_blank"
+                ><img
+                    alt="Static Badge"
+                    src="https://img.shields.io/badge/repo-171717?style=for-the-badge&logo=github"
+                /></a
+            >
+            <a
+                href="https://apod.nasa.gov/apod/archivepixFull.html"
+                title="Astronomy Picture of the Day Archive"
+                target="_blank">APOD archive ➚</a
+            >
+        </div>
+    {/if}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,41 +1,15 @@
 <script>
     import "../app.css";
+    import TopNav from "../components/TopNav.svelte";
 </script>
 
 <div
-    class="bg-stone-950 text-neutral-300 lg:flex lg:flex-col flex flex-col my-class text-base"
+    data-sveltekit-preload-data="off"
+    class="bg-stone-950 text-neutral-300 lg:flex lg:flex-col flex flex-col text-base min-w-[256px] min-h-[100dvh]"
 >
-    <div
-        class="sticky top-0 flex flex-wrap gap-4 bg-stone-900 border-b border-stone-800 bg-opacity-80 bg-clip-padding px-4 py-2 shadow-sm shadow-neutral-950 backdrop-blur-sm backdrop-filter z-50 text-neutral-400 items-center"
-    >
-        <a class="text-blue-500" href="/">home</a>
-        <a href="/apod/today">today</a>
-        <a href="/apod/day">day</a>
-        <a href="/apod/random">random</a>
-        <span class="text-stone-700">|</span>
-        <a href="https://apod.nasa.gov/apod/archivepixFull.html" target="_blank"
-            >archive ➚</a
-        >
-        <span class="flex-1" />
-        <a
-            class="border border-stone-700"
-            href="https://github.com/paulinek13/neso"
-            target="_blank"
-            ><img
-                alt="Static Badge"
-                src="https://img.shields.io/badge/repo-171717?style=for-the-badge&logo=github"
-            /></a
-        >
-    </div>
+    <TopNav />
 
     <div class="flex-1 flex justify-center">
         <slot />
     </div>
 </div>
-
-<style>
-    .my-class {
-        min-height: 100vh;
-        min-height: 100dvh;
-    }
-</style>
