b08ffd2d15275ab88c84a81a76bbfa250b0a21d7
paulinek13 <paulinek13@outlook.com>
2023-09-18 20:56:28 +0200

    fixing errors that occur for "1995-06" and the current month

--- src/routes/apod/calendar/[year]/[month]/+page.server.js
+++ src/routes/apod/calendar/[year]/[month]/+page.server.js
@@ -7,14 +7,16 @@ dayjs.extend(utc);
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
-    const startDate = `${params.year}-${params.month}-01`;
+    const startDate = ((params.year === "1995") && (params.month === "06")) ? "1995-06-16" : `${params.year}-${params.month}-01`;
     const endDate = dayjs.utc(startDate, "YYYY-MM-DD", true).endOf('month').format("YYYY-MM-DD");
+
+    const max_today = dayjs.utc(startDate, "YYYY-MM-DD", true).endOf('month').isAfter(dayjs.utc(), '') ? true : false;
 
     return await axios.request({
         timeout: 30000,
         signal: AbortSignal.timeout(30000),
         method: "GET",
-        url: `https://api.nasa.gov/planetary/apod?start_date=${startDate}&end_date=${endDate}&api_key=${process.env.NASA_API_KEY}`
+        url: `https://api.nasa.gov/planetary/apod?start_date=${startDate}&end_date=${max_today ? dayjs.utc().format("YYYY-MM-DD") : endDate}&api_key=${process.env.NASA_API_KEY}`
     }).then((res) => {
         // console.log(res.headers.get("x-ratelimit-remaining"));
 
