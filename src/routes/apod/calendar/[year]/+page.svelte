7b0add133c89e7798b1a23192415c7a3708cac02
paulinek13 <paulinek13@outlook.com>
2023-09-26 18:12:21 +0200

    fix: Resolve RollupError

--- src/routes/apod/calendar/[year]/+page.svelte
+++ src/routes/apod/calendar/[year]/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-    import CalendarYear from "../../../../components/CalendarYear.svelte";
+    import CalendarYearMonths from "./../../../../lib/components/molecules/CalendarYearMonths.svelte";
 
     export let data;
 </script>
 
 <div class="flex flex-col w-full items-center justify-center p-1">
-    <CalendarYear year={Number(data.year)} compact={false} />
+    <CalendarYearMonths year={Number(data.year)} compact={false} />
 </div>
