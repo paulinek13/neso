c461c8b0cf2c42a6cb76bb1116a5769dfac823d7
paulinek13 <paulinek13@outlook.com>
2023-10-07 20:06:27 +0200

    refactor: explicitly define runtime in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			// see the 'Deployment configuration' section below
-		})
+			runtime: 'nodejs18.x',
+		}),
 	},
 	preprocess: vitePreprocess()
 };
+
 export default config;
