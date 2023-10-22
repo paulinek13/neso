c2c3fad8c9e3fa7bf7c1f6292b39a61f3c38960e
paulinek13 <paulinek13@outlook.com>
2023-10-22 17:21:47 +0200

    build: use adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			runtime: 'nodejs18.x',
+			// runtime: 'nodejs18.x',
 		}),
 	},
 	preprocess: vitePreprocess()
