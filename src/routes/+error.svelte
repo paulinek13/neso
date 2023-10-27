08cc2e618433395c7e5f4d40ad1cc180109ff855
paulinek13 <paulinek13@outlook.com>
2023-10-27 22:24:02 +0200

    feat: improve +error page style

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,22 +2,22 @@
     import { page } from "$app/stores";
 </script>
 
-<div class="flex flex-col items-center gap-4 font-mono p-6">
+<div class="flex flex-col items-center gap-4 p-6">
     <div class="flex flex-col items-center">
         <h1 class="font-[900] text-7xl text-orange-600">
             {$page.status}
         </h1>
         <h2
-            class="text-sm text-orange-600 font-[700] tracking-wide px-4 py-1 rounded-sm"
+            class="text-orange-600 font-[700] tracking-wide px-4 py-1 rounded-sm"
         >
             {$page.error.status}
         </h2>
     </div>
-    <span class="first-letter:capitalize text-orange-300 text-center"
+    <span class="first-letter:capitalize text-orange-300 text-center text-lg"
         >{$page.error.message}</span
     >
 
-    <div class="text-xs max-w-xs text-center">
+    <div class="text-sm max-w-xs text-center">
         {#each $page.error.details as detail}
             <span>{detail}</span>
             <span />
