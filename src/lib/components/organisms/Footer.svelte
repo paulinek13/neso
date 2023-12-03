ddcb6470387a0851fbb7b7513aa79b46c7ef1648
paulinek13 <paulinek13@outlook.com>
2023-12-03 08:10:15 +0100

    revise: improve footer appearance and content placement

--- src/lib/components/organisms/Footer.svelte
+++ src/lib/components/organisms/Footer.svelte
@@ -3,29 +3,33 @@
 </script>
 
 <div
-    class="min-w-[300px] flex flex-wrap items-center justify-center p-4 gap-2 text-sm bg-stone-900 bg-opacity-50 border-t border-stone-800 text-stone-400"
+    class="min-w-[300px] bg-stone-900 bg-opacity-50 border-t border-stone-800 text-stone-400 text-xs p-4 flex justify-center"
 >
-    <a
-        href="https://github.com/paulinek13/neso"
-        target="_blank"
-        class="px-4 py-[6px] font-[500] tracking-widest text-xs border border-stone-800 text-stone-300 hover:text-stone-100 border-l-orange-900 border-r-blue-900 border-t-orange-900 border-b-blue-900"
-        >v{_VERSION}</a
-    >
-    <a
-        href="https://github.com/paulinek13/neso"
-        target="_blank"
-        class="px-4 py-[6px] font-[500] tracking-widest text-xs border border-stone-700 text-stone-300 hover:outline hover:outline-1 hover:outline-stone-700 bg-black"
-        >REPO ➚</a
-    >
-    <a
-        href="https://github.com/nasa/apod-api"
-        target="_blank"
-        class="px-4 py-[6px] font-[500] tracking-widest text-xs border border-blue-800 text-blue-300 hover:outline hover:outline-1 hover:outline-blue-800"
-        >APOD API ➚</a
-    >
-    <a
-        href="/about"
-        class="px-4 py-[6px] font-[500] tracking-widest text-xs border border-orange-800 text-orange-300 hover:outline hover:outline-1 hover:outline-orange-800"
-        >ABOUT</a
-    >
+    <div class="flex flex-col gap-1">
+        <div class="flex gap-1 justify-center">
+            <a
+                href="https://github.com/paulinek13/neso"
+                target="_blank"
+                class="block px-3 py-[5px] font-[500] tracking-widest border border-stone-700 text-stone-300 bg-black hover:outline hover:outline-1 hover:outline-stone-700"
+                >REPO ➚</a
+            >
+            <a
+                href="https://github.com/nasa/apod-api"
+                target="_blank"
+                class="block px-3 py-[5px] font-[500] tracking-widest border border-stone-700 text-blue-300 bg-black hover:outline hover:outline-1 hover:outline-blue-700"
+                >APOD API ➚</a
+            >
+            <a
+                href="/about"
+                class="block px-3 py-[5px] font-[500] tracking-widest border border-stone-700 text-orange-300 bg-black hover:outline hover:outline-1 hover:outline-orange-700"
+                >ABOUT</a
+            >
+        </div>
+        <a
+            href="https://github.com/paulinek13/neso"
+            target="_blank"
+            class="block px-3 py-[1px] font-[500] text-center text-[0.7rem] font-mono tracking-widest border border-stone-800 text-stone-300 hover:border-stone-700 hover:text-stone-200"
+            >v{_VERSION}</a
+        >
+    </div>
 </div>
