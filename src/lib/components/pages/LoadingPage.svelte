95a50bee138227eae2a12e1a2a7c3433d887161a
paulinek13 <paulinek13@outlook.com>
2023-12-04 19:26:44 +0100

    feat: add `LoadingPage` component

---
+++ src/lib/components/pages/LoadingPage.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { fade } from "svelte/transition";
+</script>
+
+<div in:fade class="m-2 h-full flex flex-col items-center justify-center">
+    <div
+        class="animate-spin p-4 aspect-square border-2 border-current border-t-transparent rounded-full text-orange-700 flex"
+        role="status"
+        aria-label="loading"
+    />
+</div>
