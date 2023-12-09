285bee8f789022988467f59d7d788bfee08aec85
paulinek13 <paulinek13@outlook.com>
2023-12-09 12:38:21 +0100

    revise: adjust margin values for various screen sizes

--- src/lib/components/templates/DefaultPageTemplate.svelte
+++ src/lib/components/templates/DefaultPageTemplate.svelte
@@ -2,6 +2,9 @@
     import { fade } from "svelte/transition";
 </script>
 
-<div in:fade={{ duration: 300 }} class="mt-2 mb-8 mx-4 h-full flex flex-col">
+<div
+    in:fade={{ duration: 300 }}
+    class="mt-2 mb-8 mx-2 sm:mx-4 2xl:mx-8 h-full flex flex-col"
+>
     <slot />
 </div>
