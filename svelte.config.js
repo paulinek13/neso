68c4ea96039b53618e36ea0f30547e7724dfe5e7
paulinek13 <paulinek13@outlook.com>
2023-12-21 10:56:11 +0100

    build: update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			// runtime: 'nodejs18.x',
+			runtime: 'nodejs18.x',
 		}),
 	},
 	preprocess: vitePreprocess()
