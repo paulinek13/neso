b0c3c1144b677f9f11af8c5229d12bb50bdf5180
paulinek13 <paulinek13@outlook.com>
2023-12-27 20:01:45 +0100

    revise: center `+error` page content

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,8 +2,10 @@
     import { page } from "$app/stores";
 </script>
 
-<div class="flex flex-col items-center gap-4 p-6">
-    <div class="flex flex-col items-center">
+<div
+    class="flex flex-col h-full items-center justify-center gap-4 text-center mx-4"
+>
+    <div class="flex flex-col items-center max-w-4xl">
         <h1 class="font-[900] text-7xl text-orange-600">
             {$page.status}
         </h1>
@@ -13,11 +15,11 @@
             {$page.error.status}
         </h2>
     </div>
-    <span class="first-letter:capitalize text-orange-300 text-center text-lg"
+    <span class="first-letter:capitalize text-orange-300 text-lg"
         >{$page.error.message}</span
     >
 
-    <div class="text-sm max-w-xs text-center">
+    <div class="text-sm max-w-xs">
         {#each $page.error.details as detail}
             <span>{detail}</span>
             <span />
